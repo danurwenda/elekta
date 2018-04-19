8a99b3e416aec9fd0927772937d31f6c629fea6e
danurwenda <dimas@danurwenda.com>
2018-04-19 23:05:43 +0700

    Add entry point into Twitter page

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 // instead of changing default route after login/register in its respective Middleware file,
 // we redirect any request to 'home' to dashboard's home url
 Route::get('home', function () {
@@ -23,8 +23,8 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 // FACEBOOK
 Route::get('sosmed/facebook', 'FacebookController@getIndex')->name('facebook');
-Route::get('sosmed/facebook/update','FacebookController@updateData');
 // TWITTER
+Route::get('sosmed/twitter', 'TwitterController@getIndex')->name('twitter');
 
 // MAINSTREAM MEDIA
 
