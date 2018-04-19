29f886d90f13951a34fc85b0d00bdf3f4443e47f
danurwenda <dimas@danurwenda.com>
2018-04-19 14:21:47 +0700

    Add Footer and credit text

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -227,7 +227,15 @@
         @endif
     </div>
     <!-- /.content-wrapper -->
-
+    <footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+            KhofifahEmil Jatim
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2018 <a href="#">Penjuru Consulting</a>.</strong> All rights reserved.
+    </footer>
+    <div class="control-sidebar-bg"></div>
 </div>
 <!-- ./wrapper -->
 @stop
